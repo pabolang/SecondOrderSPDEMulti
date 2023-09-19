aaa3ef8a24b895cb40816179f32c9aebeab39e88
pabolang <78961989+pabolang@users.noreply.github.com>
2023-09-19 11:06:36 +0200

    Add files via upload

--- R/asymp_var.R
+++ R/asymp_var.R
@@ -1,18 +1,18 @@
 #' Asymptotic variacne function
 #'
-#' Generates the asymptotic variance of the parameter estimators in the multi-dimensional SPDE model. 
+#' Generates the asymptotic variance of the parameter estimators in the multi-dimensional SPDE model.
 #' @param estimationMethod If \code{"OracleSigma"} is chosen, the funciton calculates the asymptotic variance for the oracle-volatility estimator. Therefore, provide the true parameters \code{alphaDah,sigma} of the model.
-#' If \code{"SigmaAndKappa"} is chosen, please provide the true parameters \code{alphaDash, sigma_0_squared,indexset,SG}. Note that in this case, the function returns the approximated asymptotic variance, dependent on the provided spatial coordinates. 
+#' If \code{"SigmaAndKappa"} is chosen, please provide the true parameters \code{alphaDash, sigma_0_squared,indexset,SG}. Note that in this case, the function returns the approximated asymptotic variance, dependent on the provided spatial coordinates.
 #' For additional information, see [SecondOrderSPDEMulti::estimateParametersMulti].
 #' @param d  Spatial dimension of the SPDE model.
-#' 
+#'
 #' @keywords asymptotic variance
 #' @export
 #' @seealso [SecondOrderSPDEMulti::SecondOrderSPDEMulti],[SecondOrderSPDEMulti::estimateParametersMulti].
-#' @return a value/matrix containing the asymptotic variance of the provided estimation method. 
+#' @return a value/matrix containing the asymptotic variance of the provided estimation method.
 
 #' @examples
-#' 
+#' library(data.table)
 #' d <- 2
 #' alphaDash <- 0.5
 #' sigma_0_squared <- 1
@@ -35,7 +35,7 @@ asymp_var <- function(estimationMethod,d,alphaDash=NA,sigma=NA,sigma_0_squared=N
   }
   pacman::p_load(dplyr, pbmcapply,data.table,MASS, matlib)
   numCores <- detectCores() - 1
-  
+
   if(estimationMethod == "OracleSigma"){
     res <- Upsilon(alphaDash,d)*sigma^4
     return(res)
@@ -44,14 +44,14 @@ asymp_var <- function(estimationMethod,d,alphaDash=NA,sigma=NA,sigma_0_squared=N
     U <- Upsilon(alphaDash,d)
     I <- -diag(d+1)
     I[1,1] <- sigma_0_squared
-    
+
     S <- SG[indexset,]
     M <- length(indexset)
     ones <- rep(1,M)
     X <- as.matrix(cbind(ones,S))
     Sigma <- crossprod(X)/M
     res <- I %*% inv(Sigma) %*% I
-    
+
     return(res*U)
   }
 }
